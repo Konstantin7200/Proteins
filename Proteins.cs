018748ecabb29f6bf36902ecc9cce24aebc78afb
Konstantin Sl <kostatop2@gmail.com>
2025-10-02 14:18:59 +0300

    Made the most of the logic,working on the output

--- Proteins.cs
+++ Proteins.cs
@@ -1,4 +1,6 @@
 
+using System.Xml.Linq;
+
 namespace Proteins
 {
     interface IHandleInput
@@ -7,27 +9,39 @@ namespace Proteins
     }
     interface IHandleLogic
     {
-        List<Protein> search(string substr,List<Protein> AllProteins);
+        List<Protein> search(string substr,List<Protein> allProteins);
         int diff(Protein protein1, Protein protein2);
         char mode(string proteinName, List<Protein> allProteins);
     }
     interface IHandleOutput
     {
-
+        
+    }
+    class OutputHandler
+    {
+        void output(string command, int commandNum, string result)
+        {
+            string pathname = "genedata.txt";
+            string splitter = "-------------------------------------\n";
+            File.WriteAllText(pathname, splitter);
+            File.WriteAllText(pathname, "00" + commandNum + "\t" + command + "\n");
+            File.WriteAllText(pathname, result);
+            File.WriteAllText(pathname, splitter);
+        }
     }
     class LogicHandler
     {
-        public List<Protein> search(string substr, List<Protein> AllProteins)
+        static public List<Protein> search(string substr, List<Protein> allProteins)
         {
             List<Protein> proteins = new List<Protein>();
-            for (int i = 0; i < AllProteins.Count; i++)
+            for (int i = 0; i < allProteins.Count; i++)
             {
-                if (AllProteins[i].getAminoacid().Contains(substr))
-                    proteins.Add(AllProteins[i]);
+                if (allProteins[i].getAminoacid().Contains(substr))
+                    proteins.Add(allProteins[i]);
             }
             return proteins;
         }
-        public int diff(Protein protein1, Protein protein2)
+        static public int diff(Protein protein1, Protein protein2)
         {
             string aminoacid1 = protein1.getAminoacid();
             string aminoacid2 = protein2.getAminoacid();
@@ -39,7 +53,7 @@ namespace Proteins
             return count;
         }
 
-        public char mode(string proteinName,List<Protein> allProteins)
+        static public char mode(string proteinName,List<Protein> allProteins)
         {
             string aminoacid="";
             Dictionary<char, int> dict = new Dictionary<char, int>();
@@ -53,18 +67,109 @@ namespace Proteins
             }
             for(int i=0;i<aminoacid.Length;i++)
             {
-                aminoacid.ToDictionary
+                if (!dict.Keys.Contains(aminoacid[i]))
+                    dict.Add(aminoacid[i], 0);
+                else dict[aminoacid[i]]++;
+            }
+            int max = dict.Values.Max();
+            char result = 'Z';
+            foreach(char symbol in dict.Keys)
+            {
+                if (dict[symbol] == max && symbol < result)
+                    result = symbol;
             }
 
-            return ' ';
+            return result;
+        }
+    }
+
+    class ResultFormer()
+    {
+        public static string formFromSearch(List<Protein> allProteins)
+        {
+            string result="organism"+"                 "+"protein\n";
+
+            if (allProteins.Count == 0)
+                result+= "NOT FOUND/n";
+            for(int i=0;i<allProteins.Count;i++)
+            {
+                result += allProteins[i].getOrganism + "                 " + allProteins[i].getAminoacid() + "\n";
+            }
+            return result;
+        }
+        public static string formFromDiff(int difference,string proteinName1,string proteinName2)
+        {
+            string result="";
+            if (difference == -1)
+            {
+                result += "Missing:" + proteinName1;
+            }
+            else if (difference == -2)
+            {
+                result += "Missing:" + proteinName2;
+            }
+            else if (difference == -1)
+            {
+                result += "Missing:" + proteinName1 + "and" + proteinName2;
+            }
+            else
+            {
+                result += "amino-acids difference:\n" + difference;
+            }
+            return result;
+        }
+        public static string formFromMode()
+        {
+            string result="";
+
+
+
+            return result;
+        }
+
+    }
+
+    class CommandCenter
+    {
+        public void analyzeCommand(string commandString,List<Protein> allProteins)
+        {
+            string[] command = commandString.Split('\t');
+            if (command[0]=="search")
+            {
+                LogicHandler.search(Protein.decodeAminoacid(command[1]), allProteins);
+            }
+            else if (command[0]=="diff")
+            {
+                string name1 = command[1];
+                string name2 = command[2];
+                Protein protein1=null, protein2=null;
+                foreach (Protein protein in allProteins)
+                {
+                    if(protein.getName()==name1)
+                    {
+                        protein1 = protein;
+                    }
+                    else if(protein.getName()==name2)
+                    {
+                        protein2 = protein;
+                    }
+                }
+                if(protein1!=null&&protein2!=null)
+                LogicHandler.diff(protein1, protein2);
+            }
+
+            else if (command[0] == "mode")
+            {
+                LogicHandler.mode(command[1], allProteins);
+            }
         }
     }
     class InputHandler : IHandleInput
     {
         public string[] handleInput(string pathname)
         {
-            string allText = File.ReadAllText("pathname");
-            return allText.Split();
+            string allText = File.ReadAllText(pathname);
+            return allText.Split('\n');
         }
     }
     class Protein
@@ -80,14 +185,20 @@ namespace Proteins
         {
             return name;
         }
-        Protein(string args)
+        public string getOrganism()
+        {
+            return organism;
+        }
+        public Protein(string args)
         {
             string[] splitted = args.Split("\t");
-            name = splitted[0];
-            organism = splitted[1];
-            aminoacid=decodeAminoacid(splitted[2]);
+            
+                name = splitted[0];
+                organism = splitted[1];
+                aminoacid = decodeAminoacid(splitted[2]);
+            
         }
-        string decodeAminoacid(string aminoacid)
+        static public string decodeAminoacid(string aminoacid)
         {
             string result="";
             for(int i=0;i<aminoacid.Length;i++)
@@ -107,10 +218,20 @@ namespace Proteins
     {
         public static void Main()
         {
-            
+            InputHandler inputHandler = new InputHandler();
+            string[] proteins = inputHandler.handleInput("sequences.0.txt");
+            string[] commands=inputHandler.handleInput("commands.0.txt");
+
+            List<Protein> allProteins = new List<Protein>();
+            for (int i = 0; i < proteins.Length-1; i++)
+            {
+                allProteins.Add(new Protein(proteins[i]));
+            }
+            CommandCenter commandCenter = new CommandCenter();
+            for(int i=0;i<commands.Length;i++)
+                commandCenter.analyzeCommand(commands[i], allProteins);
+
         }
     }
 
-
-    
 }
